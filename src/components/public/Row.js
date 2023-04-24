8569a0f5cfc20236e2f44871faba60afcb8a6197
martin machado <martinmachado_86@hotmail.com>
2023-04-24 14:05:44 -0300

    funcionalidad boton click

--- src/components/public/Row.js
+++ src/components/public/Row.js
@@ -1,19 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export const Row = ({ el, setEditTask, deleteTask }) => {
-    let { id } = el
+  let { id } = el;
+  const [click, setClik] = useState(false);
+
+
+
+  const clickChek = () => {
     
-    return (
-        <tr>
+    setClik(!click);
 
-            <td><input className="form-check-input me-1" type="checkbox"  id="firstCheckbox" /></td>
-            <td>{el.title}</td>
-            <td>{el.description}</td>
-            <td>{el.deadLine}</td>
-            <td><button className="modificar" onClick={() => setEditTask(el)}>Modificar</button></td>
-            <td><button className="eliminar" onClick={() => deleteTask(id)}>Eliminar</button></td>
+    if (!click) {
+      setClik("Tarea realizada");
 
-        </tr>
-    )
+    } else {
+      setClik("");
+    }
+  }
+
+  return (
+    <tr>
+
+      <td ><input className="form-check-input me-1" type="checkbox" id="firstCheckbox" onClick={clickChek} /></td>
+      <td>{el.title}</td>
+      <td>{el.description}</td>
+      <td>{el.deadLine}</td>
+      <td >{click}</td>
+      <td><button className="modificar" onClick={() => setEditTask(el)}>Modificar</button></td>
+      <td><button className="eliminar" onClick={() => deleteTask(id)}>Eliminar</button></td>
+
+    </tr>
+  )
 }
